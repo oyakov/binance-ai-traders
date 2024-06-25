aab6b7f053e1180998361d17736ecac3af5f75e9
oyakov <oyakovinbox@gmail.com>
2024-06-26 00:20:49 +0200

    Work on the scheduler

--- subsystem/scheduler.py
+++ subsystem/scheduler.py
@@ -2,6 +2,7 @@ from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from db.config import get_db
 from db.model.calendar_data import CalendarData
 from service.telegram_service import send_telegram_message
+from sqlalchemy.future import select
 from aiogram import Bot
 import logging
 
@@ -14,17 +15,23 @@ logger = logging.getLogger(__name__)
 async def check_calendar(bot: Bot):
     logger.info("job launched")
     async with get_db() as session:
-        logger.info("session obtaned")
-        calendar_entries = await session.execute(CalendarData.select())
-        calendar_entries = calendar_entries.fetchall()
-        logger.info(f"calendar entries: {calendar_entries}")
-        for entry in calendar_entries:
-            # Construct the message to be sent
-            message = f"Entry {entry}"
-            logger.info(f"Send telegram message for entry {entry}")
-            await send_telegram_message(bot, entry.user_id, message)
+        logger.info(f"session obtaned {session}")
+        async with session.begin():        
+            calendar_entries = await session.execute(select(CalendarData))
+            calendar_entries: list[CalendarData] = calendar_entries.fetchall()
+            logger.info(f"calendar entries: {calendar_entries}")
+            for entry in calendar_entries:
+                # Construct the message to be sent
+                message = f"Entry {entry}"
+                for key, value in entry.__dict__.items():
+                    if not key.startswith('_'):
+                        print(f"{key}: {value}")
+                logger.info(f"Send telegram message for entry {entry}")
+                user_id = await entry.awaitable_attrs.username
+                await send_telegram_message(bot, user_id, message)
 
 async def init_scheduler(bot: Bot):
+
     logger.info("Initailize the scheduler")
     scheduler = AsyncIOScheduler()
     scheduler.add_job(check_calendar, 'interval', args=[bot], minutes=2)
