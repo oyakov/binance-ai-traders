db79d7953e55763b358a5eb3de3565f9daedb866
oyakov <oyakovinbox@gmail.com>
2024-06-27 02:25:34 +0200

    First working concept of the periodic spamming feature

--- subsystem/scheduler.py
+++ subsystem/scheduler.py
@@ -1,10 +1,9 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from db.config import get_db
 from db.model.calendar_data import CalendarData
 from service.telegram_service import send_telegram_message
-from sqlalchemy.future import select
 from aiogram import Bot
-import logging
+from db.config import *
+from sqlalchemy import text
 
 ############################################################################
 # logging
@@ -12,28 +11,32 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 ############################################################################
 
+
 async def check_calendar(bot: Bot):
     logger.info("job launched")
     async with get_db() as session:
-        logger.info(f"session obtaned {session}")
-        async with session.begin():        
-            calendar_entries = await session.execute(select(CalendarData))
-            calendar_entries: list[CalendarData] = calendar_entries.fetchall()
+        logger.info(f"session obtained {session}")
+        async with session.begin():
+            stmt = text('''
+            SELECT 
+                cd.username as username,
+                cd.data as data
+            from calendar_data cd''')
+            calendar_entries = await session.execute(stmt)
+            calendar_entries = calendar_entries.fetchall()
             logger.info(f"calendar entries: {calendar_entries}")
-            for entry in calendar_entries:
+            for username, data in calendar_entries:
                 # Construct the message to be sent
-                message = f"Entry {entry}"
-                for key, value in entry.__dict__.items():
-                    if not key.startswith('_'):
-                        print(f"{key}: {value}")
-                logger.info(f"Send telegram message for entry {entry}")
-                user_id = await entry.awaitable_attrs.username
-                await send_telegram_message(bot, user_id, message)
+                logger.info(f"Send telegram message for username {username}")
+                await send_telegram_message(bot, '-1002060021902', data)
+
 
 async def init_scheduler(bot: Bot):
-
-    logger.info("Initailize the scheduler")
+    """
+    Initialize the periodic job for sending messages
+    """
+    logger.info("Initialize the scheduler job")
     scheduler = AsyncIOScheduler()
-    scheduler.add_job(check_calendar, 'interval', args=[bot], minutes=2)
+    scheduler.add_job(check_calendar, 'interval', args=[bot], minutes=1)
     scheduler.start()
     logger.info("Scheduler initialized")
